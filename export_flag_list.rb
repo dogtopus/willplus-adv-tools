71e0934499a87a28e37c393a3865cfdb0a3d749d
dogtopus <dogtopus@users.noreply.github.com>
2020-04-12 01:33:53 -0300

    [config_gen] Export flag list inside `python early` block following loading policy changes in will_flag_bank.

--- export_flag_list.rb
+++ export_flag_list.rb
@@ -3,8 +3,8 @@
 require_relative 'op2rpy_settings'
 
 File.open(ARGV[0], 'w') do |f|
-    f.puts('init -899:')
-    f.puts('  define WILL_FLAG_NAMES = {')
+    f.puts('python early:')
+    f.puts('  WILL_FLAG_NAMES = {')
     O2RSettings::FLAG_TABLE.each do |addr, prop|
         next if prop[0].nil?
         # TODO escaping?
