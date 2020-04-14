903393607746ec70de1f015537992ba3c11e57b8
dogtopus <dogtopus@users.noreply.github.com>
2020-04-13 23:42:28 -0300

    [wsc2scr.py] Better Python version

--- wsc2scr.py
+++ wsc2scr.py
@@ -6,15 +6,8 @@ if len(sys.argv)<3:
 	print('Usage: wsc2scr <Input File> <Output File>')
 	sys.exit(1)
 
-try:
-	fin=open(sys.argv[1],'rb')
-	buffer1=fin.read()
-	buffer2=''
-	for i in buffer1:
-		buffer2+=chr((ord(i)>>2)|((ord(i)<<6)&255))
-	fout=open(sys.argv[2],'wb')
-	fout.write(buffer2)
-finally:
-	fin.close()
-	fout.close()
-
+with open(sys.argv[1], 'rb') as fin, open(sys.argv[2], 'wb') as fout:
+    tmp = bytearray(1)
+    while fin.readinto(tmp) == len(tmp):
+        tmp[0] = ((tmp[0] >> 2) | (tmp[0] << 6)) & 0xff
+        fout.write(tmp)
