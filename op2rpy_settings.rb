ef3aef0471bdb925f4409092f7d6bdf321f7114a
dogtopus <dogtopus@users.noreply.github.com>
2020-04-17 01:25:37 -0300

    [op2rpy_settings] Add a category field to flag table

--- op2rpy_settings.rb
+++ op2rpy_settings.rb
@@ -38,78 +38,90 @@ module O2RSettings
         [1000, 2999, 'persistent.will_flagbank'],
     ]
 
+    # addr => [name, inclusion_policy, category]
+    # addr: 16-bit address of the flag.
+    # name: The name of the variable. Setting to nil will allow other fields (e.g. inclusion policy) to be specified but keep the flag anonymous.
+    # inclusion_policy:
+    #   Flag::INCLUDE: Include the flag in the emitted .rpy files with full read/write access.
+    #   Flag::EXCLUDE: Exclude the flag in the emitted .rpy files. CJMPs that use it will always be evaluated to false.
+    #   Flag::HINT: Similar to Flag::EXCLUDE but inserts a comment instead of completely omitting it.
+    # category:
+    #   FlagCategory::UNCATEGORIZED: Uncategorized.
+    #   FlagCategory::STORY: Story related (i.e. used as a branching condition during the story-telling)
+    #   FlagCategory::UNLOCK: Unlocks gallery/event entries
+    #   FlagCategory::SYSTEM: Flags used by the infrastructure (non-story) code as temporary or persistent variables.
     FLAG_TABLE = {
-        2 => ['decided_to_build_dessert_house', Flag::INCLUDE],
-        3 => ['gem_37564_first_seen', Flag::INCLUDE],
-        4 => ['gem_37564_ask_guillered', Flag::INCLUDE],
-        7 => ['is_blue_r0', Flag::INCLUDE],
-        43 => ['asked_roddy_for_dinner', Flag::INCLUDE],
-        47 => ['met_ioan_r0_d4_am', Flag::INCLUDE],
-        48 => ['found_book_r0_d4_noon', Flag::INCLUDE],
-        170 => ['aff_roddy', Flag::INCLUDE], # As of Route0
-        171 => ['aff_ioan', Flag::INCLUDE], # As of Route0
-        172 => ['aff_greg', Flag::INCLUDE],
-        174 => ['aff_dag', Flag::INCLUDE],
-        180 => ['gem_37564', Flag::INCLUDE], # As of Route0
-        181 => ['house', Flag::INCLUDE], # As of Route0
-        199 => ['gem_37564_more_people_ded', Flag::INCLUDE],
-        200 => ['gem_37564_dad_ded', Flag::INCLUDE],
-        201 => ['gem_37564_mom_ded', Flag::INCLUDE],
-        202 => ['gem_37564_brother_ded', Flag::INCLUDE],
-        203 => ['gem_37564_sister_ded', Flag::INCLUDE],
-        204 => ['gem_37564_neighbor_girl_ded', Flag::INCLUDE],
-        205 => ['gem_37564_neighbor_boy_ded', Flag::INCLUDE],
-        206 => ['gem_37564_classmate_0_ded', Flag::INCLUDE],
-        207 => ['gem_37564_classmate_1_ded', Flag::INCLUDE],
-        208 => ['gem_37564_classmate_2_ded', Flag::INCLUDE],
-        209 => ['gem_37564_classmate_3_ded', Flag::INCLUDE],
-        211 => ['disp_list', Flag::INCLUDE], # As of Route0
-        700 => ['cgdisp_page_num', Flag::HINT],
-        709 => ['has_bgm', Flag::HINT],
-        720 => ['current_event_id', Flag::INCLUDE],
-        723 => ['has_bg', Flag::HINT],
-        762 => ['cutscene_index', Flag::INCLUDE],
-        763 => ['cutscene_unskippable', Flag::INCLUDE],
-        765 => ['ctr_cg', Flag::HINT], # May not useful since we have len() TODO is it just some temp variable?
-        850 => ['has_opt_0', Flag::HINT],
-        851 => ['has_opt_1', Flag::HINT],
-        852 => ['has_opt_2', Flag::HINT],
-        853 => ['has_opt_3', Flag::HINT],
-        995 => ['in_event_view_mode', Flag::INCLUDE],
-        996 => ['performing_transition', Flag::HINT],
-        970 => ['gem_maturity', Flag::INCLUDE], # As of Route0
-        990 => ['list_related', Flag::HINT], # Purpose unknown
-        1004 => ['seen_ending_cutscene', Flag::INCLUDE],
-        1007 => ['first_run', Flag::HINT],
-        1008 => ['clear_r0', Flag::INCLUDE],
-        1009 => ['clear_r2', Flag::INCLUDE],
-        1010 => ['clear_r3', Flag::INCLUDE],
-        1014 => ['clear_r0_ioan', Flag::INCLUDE],
-        1430 => ['unlock_event_ioan_end_0', Flag::INCLUDE],
-        1431 => ['unlock_event_ioan_end_1', Flag::INCLUDE],
-        1432 => ['unlock_event_ioan_end_2', Flag::INCLUDE],
-        1433 => ['unlock_event_roddy_end_0', Flag::INCLUDE],
-        1434 => ['unlock_event_roddy_end_1', Flag::INCLUDE],
-        1435 => ['unlock_event_dick_captured_0', Flag::INCLUDE],
-        1436 => ['unlock_event_dick_captured_1', Flag::INCLUDE],
-        1437 => ['unlock_event_dick_captured_2', Flag::INCLUDE],
-        1438 => ['unlock_event_dag_end_0', Flag::INCLUDE],
-        1439 => ['unlock_event_dag_end_1', Flag::INCLUDE],
-        1440 => ['unlock_event_sergi_end_0', Flag::INCLUDE],
-        1441 => ['unlock_event_sergi_end_1', Flag::INCLUDE],
-        1442 => ['unlock_event_cornel_dick', Flag::INCLUDE],
-        1443 => ['unlock_event_cornel_end_0', Flag::INCLUDE],
-        1444 => ['unlock_event_cornel_end_1', Flag::INCLUDE],
-        1445 => ['unlock_event_cornel_end_2', Flag::INCLUDE],
-        1446 => ['unlock_event_greg_end_0', Flag::INCLUDE],
-        1447 => ['unlock_event_greg_end_1', Flag::INCLUDE],
-        1448 => ['unlock_event_greg_end_encore', Flag::INCLUDE],
-        1449 => ['unlock_event_guillered_0', Flag::INCLUDE],
-        1450 => ['unlock_event_guillered_1', Flag::INCLUDE],
-        1451 => ['unlock_event_guillered_2', Flag::INCLUDE],
-        1452 => ['unlock_event_cornel_oswald', Flag::INCLUDE],
-        1453 => ['unlock_event_aby_captured_0', Flag::INCLUDE],
-        1454 => ['unlock_event_aby_captured_1', Flag::INCLUDE],
-        10007 => ['system_keycode', Flag::HINT],
+        2 => ['decided_to_build_dessert_house', Flag::INCLUDE, FlagCategory::STORY],
+        3 => ['gem_37564_first_seen', Flag::INCLUDE, FlagCategory::STORY],
+        4 => ['gem_37564_ask_guillered', Flag::INCLUDE, FlagCategory::STORY],
+        7 => ['is_blue_r0', Flag::INCLUDE, FlagCategory::STORY],
+        43 => ['asked_roddy_for_dinner', Flag::INCLUDE, FlagCategory::STORY],
+        47 => ['met_ioan_r0_d4_am', Flag::INCLUDE, FlagCategory::STORY],
+        48 => ['found_book_r0_d4_noon', Flag::INCLUDE, FlagCategory::STORY],
+        170 => ['aff_roddy', Flag::INCLUDE, FlagCategory::STORY], # As of Route0
+        171 => ['aff_ioan', Flag::INCLUDE, FlagCategory::STORY], # As of Route0
+        172 => ['aff_greg', Flag::INCLUDE, FlagCategory::STORY],
+        174 => ['aff_dag', Flag::INCLUDE, FlagCategory::STORY],
+        180 => ['gem_37564', Flag::INCLUDE, FlagCategory::STORY], # As of Route0
+        181 => ['house', Flag::INCLUDE, FlagCategory::STORY], # As of Route0
+        199 => ['gem_37564_more_people_ded', Flag::INCLUDE, FlagCategory::STORY],
+        200 => ['gem_37564_dad_ded', Flag::INCLUDE, FlagCategory::STORY],
+        201 => ['gem_37564_mom_ded', Flag::INCLUDE, FlagCategory::STORY],
+        202 => ['gem_37564_brother_ded', Flag::INCLUDE, FlagCategory::STORY],
+        203 => ['gem_37564_sister_ded', Flag::INCLUDE, FlagCategory::STORY],
+        204 => ['gem_37564_neighbor_girl_ded', Flag::INCLUDE, FlagCategory::STORY],
+        205 => ['gem_37564_neighbor_boy_ded', Flag::INCLUDE, FlagCategory::STORY],
+        206 => ['gem_37564_classmate_0_ded', Flag::INCLUDE, FlagCategory::STORY],
+        207 => ['gem_37564_classmate_1_ded', Flag::INCLUDE, FlagCategory::STORY],
+        208 => ['gem_37564_classmate_2_ded', Flag::INCLUDE, FlagCategory::STORY],
+        209 => ['gem_37564_classmate_3_ded', Flag::INCLUDE, FlagCategory::STORY],
+        211 => ['disp_list', Flag::INCLUDE, FlagCategory::SYSTEM], # As of Route0
+        700 => ['cgdisp_page_num', Flag::HINT, FlagCategory::SYSTEM],
+        709 => ['has_bgm', Flag::HINT, FlagCategory::SYSTEM],
+        720 => ['current_event_id', Flag::INCLUDE, FlagCategory::SYSTEM],
+        723 => ['has_bg', Flag::HINT, FlagCategory::SYSTEM],
+        762 => ['cutscene_index', Flag::INCLUDE, FlagCategory::SYSTEM],
+        763 => ['cutscene_unskippable', Flag::INCLUDE, FlagCategory::SYSTEM],
+        765 => ['ctr_cg', Flag::HINT, FlagCategory::SYSTEM], # May not useful since we have len() TODO is it just some temp variable?
+        850 => ['has_opt_0', Flag::HINT, FlagCategory::SYSTEM],
+        851 => ['has_opt_1', Flag::HINT, FlagCategory::SYSTEM],
+        852 => ['has_opt_2', Flag::HINT, FlagCategory::SYSTEM],
+        853 => ['has_opt_3', Flag::HINT, FlagCategory::SYSTEM],
+        995 => ['in_event_view_mode', Flag::INCLUDE, FlagCategory::SYSTEM],
+        996 => ['performing_transition', Flag::HINT, FlagCategory::SYSTEM],
+        970 => ['gem_maturity', Flag::INCLUDE, FlagCategory::STORY], # As of Route0
+        990 => ['list_related', Flag::HINT, FlagCategory::SYSTEM], # Purpose unknown
+        1004 => ['seen_ending_cutscene', Flag::INCLUDE, FlagCategory::SYSTEM],
+        1007 => ['first_run', Flag::HINT, FlagCategory::SYSTEM],
+        1008 => ['clear_r0', Flag::INCLUDE, FlagCategory::STORY],
+        1009 => ['clear_r2', Flag::INCLUDE, FlagCategory::STORY],
+        1010 => ['clear_r3', Flag::INCLUDE, FlagCategory::STORY],
+        1014 => ['clear_r0_ioan', Flag::INCLUDE, FlagCategory::STORY],
+        1430 => ['unlock_event_ioan_end_0', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1431 => ['unlock_event_ioan_end_1', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1432 => ['unlock_event_ioan_end_2', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1433 => ['unlock_event_roddy_end_0', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1434 => ['unlock_event_roddy_end_1', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1435 => ['unlock_event_dick_captured_0', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1436 => ['unlock_event_dick_captured_1', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1437 => ['unlock_event_dick_captured_2', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1438 => ['unlock_event_dag_end_0', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1439 => ['unlock_event_dag_end_1', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1440 => ['unlock_event_sergi_end_0', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1441 => ['unlock_event_sergi_end_1', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1442 => ['unlock_event_cornel_dick', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1443 => ['unlock_event_cornel_end_0', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1444 => ['unlock_event_cornel_end_1', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1445 => ['unlock_event_cornel_end_2', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1446 => ['unlock_event_greg_end_0', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1447 => ['unlock_event_greg_end_1', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1448 => ['unlock_event_greg_end_encore', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1449 => ['unlock_event_guillered_0', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1450 => ['unlock_event_guillered_1', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1451 => ['unlock_event_guillered_2', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1452 => ['unlock_event_cornel_oswald', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1453 => ['unlock_event_aby_captured_0', Flag::INCLUDE, FlagCategory::UNLOCK],
+        1454 => ['unlock_event_aby_captured_1', Flag::INCLUDE, FlagCategory::UNLOCK],
+        10007 => ['system_keycode', Flag::HINT, FlagCategory::SYSTEM],
     }
 end
--- op2rpy_settings_enum.rb
+++ op2rpy_settings_enum.rb
@@ -1,9 +1,22 @@
 #!/usr/bin/env ruby
 module O2RSettingsEnum
     module Flag
+        # Include the flag in the emitted .rpy files with full read/write access.
         INCLUDE = 1
+        # Exclude the flag in the emitted .rpy files. CJMPs that use it will always be evaluated to false.
         EXCLUDE = 0
+        # Similar to Flag::EXCLUDE but inserts a comment instead of completely omitting it.
         HINT = 2
+    end
+    module FlagCategory
+        # Uncategorized.
+        UNCATEGORIZED = 0
+        # Story related (i.e. used as a branching condition during the story-telling)
+        STORY = 1
+        # Unlocks gallery/event entries
+        UNLOCK = 2
+        # Flags used by the infrastructure (non-story) code as temporary or persistent variables.
+        SYSTEM = 3
     end
     module SubstType
         # Ignore this entry
