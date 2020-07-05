5799518d08e002ab6caf5cc3b5aa47dbd025420a
dogtopus <dogtopus@users.noreply.github.com>
2020-07-05 15:47:49 -0300

    [wsc2scr.py] Add reverse option

--- wsc2scr.py
+++ wsc2scr.py
@@ -2,12 +2,20 @@
 
 import sys
 
-if len(sys.argv)<3:
-	print('Usage: wsc2scr <Input File> <Output File>')
+if len(sys.argv) < 3:
+	print('Usage: wsc2scr <Input File> <Output File> [-r]')
 	sys.exit(1)
+
+if len(sys.argv) > 3 and sys.argv[3] == '-r':
+    reverse = True
+else:
+    reverse = False
 
 with open(sys.argv[1], 'rb') as fin, open(sys.argv[2], 'wb') as fout:
     tmp = bytearray(1)
     while fin.readinto(tmp) == len(tmp):
-        tmp[0] = ((tmp[0] >> 2) | (tmp[0] << 6)) & 0xff
+        if reverse:
+            tmp[0] = ((tmp[0] << 2) | (tmp[0] >> 6)) & 0xff
+        else:
+            tmp[0] = ((tmp[0] >> 2) | (tmp[0] << 6)) & 0xff
         fout.write(tmp)
